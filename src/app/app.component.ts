4d4defa4b925276de8e7bf530ceebfc7f68cda54
harshkoralwala <harshkoralwala@hotmail.com>
2024-05-03 16:53:28 +0530

    -added comments as per usages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,20 @@ import { Component, computed, effect, signal } from '@angular/core';
 })
 export class AppComponent {
 
-  //Number Signal
+  //Number Signal #1
   count = signal<number>(3);
-  //String Signal
+
+  //String Signal #2
   name = signal('');
-  //Object Signal
+
+  //Object Signal #3 (Mutate)
+  //Object Signal #4 (Set)
   person = signal({ firstname: 'bacancy', lastName: 'Tech' });
   person1 = signal(this.person());
 
-  //Computed Signal
-  fName = signal('A');
-  lName = signal('Z');
+  // Computed Signal #5
+  fName = signal('');
+  lName = signal('');
   fullName = computed(() => this.fName() + ' ' + this.lName());
 
   //Computed Signal + example of Cached result
@@ -30,8 +33,11 @@ export class AppComponent {
     }
   });
 
+  //Complex DataType #7
+  numbers = signal([1, 2, 5, 7, 8, 3, 1, 4, 8, 3, 33, 9]);
 
   constructor() {
+    //Effects #6
     effect(() => {
       console.log("New values", this.fName(), this.lName());
     })
@@ -53,15 +59,23 @@ export class AppComponent {
     this.name.set('bar');
   }
 
-  firstName() {
+  firstName_mutate() {
     //Motifies and returns Existing Object,hence all reference will be updated
     this.person.mutate(t => { t.firstname = 'foo' });
+  }
+
+  lastName_mutate() {
+    this.person.mutate(t => { t.lastName = 'bar' });
+  }
+
+  firstName_set() {
     //Returns new Object,So new memory allocation,Other variable still refers old memory references
     this.person.set({ firstname: 'Apple', lastName: 'Banana' });
   }
 
-  lastName() {
-    this.person.mutate(t => { t.lastName = 'bar' });
+  lastName_set() {
+    this.person.set({ firstname: 'Car', lastName: 'Bike' });
+
   }
 
   test() {
